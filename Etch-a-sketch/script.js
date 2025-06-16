f2cd79331626fbc1537952c9e50071c7399a8ca6
Jaden <jaysnyderrr@gmail.com>
2025-06-16 11:05:04 -0400

    Updated index.html, script.js, styles.css (Configured functions for gridSize, black / rainbow drawing colors, etc.)

--- Etch-a-sketch/script.js
+++ Etch-a-sketch/script.js
@@ -1,14 +1,76 @@
-const grid = document.getElementById("grid-container");
-const tools = document.getElementById("tools-container");
+// Global variable for Drawing Color
+let color = "black";
 
-const default_color = "#333333";
-const default_size = 16;
+// INITIALIZATION
+document.addEventListener("DOMContentLoaded", function () {
+  // Default Size of board
+  createBoard(5);
 
-// CHANGE GRID SIZE - Prompt user to enter grid prefered grid size
-// Must be x >= 5 and x <= 100
+  // User adjusts Size of board
+  let changeGridSize = document.querySelector("#change-grid");
+  changeGridSize.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
 
-// SOLID BLACK COLOR
+  // Change color back to solid black
+  let colorBlack = document.querySelector("#color-black");
+  colorBlack.addEventListener("click", function () {
+    solidBlack();
+  });
+});
 
+// CREATE BOARD - Add specified number of divs to grid
+function createBoard(size) {
+  let board = document.querySelector(".board");
+
+  //Adjust grid size to given size value
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  // Create divs inside grid/board
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "white";
+    // COLOR THE DIV
+    div.addEventListener("mouseover", colorDiv);
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
+
+// OBTAIN SIZE OF GRID FROM USER
+function getSize() {
+  let input = prompt("Input Grid Size: \n Must be between 5 - 100");
+  let message = document.querySelector("#message");
+
+  // Adjust grid size message based on input
+  if (input == "") {
+    message.innerHTML = "Provide a number to change grid size";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Provide a number between 1 - 100";
+  } else {
+    message.innerHTML = "";
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == "rainbow") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+// // SOLID BLACK COLOR
+// function solidBlack() {
+//   let blah = prompt("SUPPPPPPPPPP");
+// }
 // RAINBOW COLOR (RGB Random)
 
 // SHADE COLOR (Progressively get darker until black)
